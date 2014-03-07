a49556844e247a7c21d8176ca2bf9f69c7e6d394
Keith Webber <keith.e.webber@gmail.com>
2014-03-07 17:44:38 -0500

    Finished the Rectangle Feature

--- lib/Rectangle.rb
+++ lib/Rectangle.rb
@@ -1,1 +1,18 @@
 require_relative 'shapes_helper.rb'
+
+class Rectangle
+  def initialize(length, width = length)
+    raise("length and width must be positve numbers") if length <= 0 || width <= 0
+    @length = length
+    @width = width
+  end
+
+  def area
+    @length * @width
+  end
+
+  def perimeter
+    2* (@length + @width)
+  end
+
+end 
--- spec/Rectangle_spec.rb
+++ spec/Rectangle_spec.rb
@@ -1,1 +1,31 @@
 require_relative 'spec_helper.rb'
+
+describe Rectangle do
+  
+  it "takes 2 arguements" do
+    expect(Rectangle.new(2,1).class).to eq(Rectangle)    
+  end
+
+  it "throws an error if  1 out of 2 arguements are not positive" do
+    expect{Rectangle.new(2,-1)}.to raise_error
+  end
+
+  it "throws an error if  2 out of 2 arguements are not positive" do
+    expect{Rectangle.new(-2,-1)}.to raise_error
+  end
+  it "takes 1 arguement and creates a Rectangle with equal lenght sides AKA a square" do
+    expect(Rectangle.new(1).class).to eq(Rectangle)
+  end
+  it "does not take arguements that are non positive" do
+    expect {Rectangle.new("Cards against humanity")}.to raise_error
+  end
+  
+  it "calculates the area" do
+    expect(Rectangle.new(2,3).area).to eq(6)
+  end
+
+  it "calculates the perimeter" do
+    expect(Rectangle.new(2,3).perimeter).to eq(10)
+  end
+
+end
