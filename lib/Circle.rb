d3f2e231a56a4c7f427ae216e5a0e215b8886345
Keith Webber <keith.e.webber@gmail.com>
2014-03-07 17:26:47 -0500

    tests for Circle

--- lib/Circle.rb
+++ lib/Circle.rb
@@ -1,1 +1,26 @@
 require_relative 'shapes_helper.rb'
+
+class Circle
+  attr_reader :radius
+  def initialize(radius)
+    raise("non-negative numbers only please") if radius <= 0
+    @radius = radius
+  end
+
+  def diameter
+    @radius * 2
+  end
+
+  def circumference
+    (@radius * 2 * Math::PI).round(5)
+  end
+
+  def area(words= false)
+    if words
+      "PI #{@radius}in^2"
+    else
+      (Math::PI * @radius ** 2).round(5)
+    end
+  end
+  
+end
--- spec/Circle_spec.rb
+++ spec/Circle_spec.rb
@@ -2,6 +2,54 @@ require_relative 'spec_helper.rb'
 
 
 describe Circle do
+  
+  #math nerding it out
+  let(:unit_circle) {Circle.new(1)}
+
+  it "takes only 1 argument as radius" do
+    expect(Circle.new(2).class).to eq(Circle)
+  end
+
+  it "takes only 1 that is positive argument as radius" do
+    expect{Circle.new(2,3)}.to raise_error
+  end    
+
+  #DONT WE ALL LOVE PI
+  it "takes only 1 that is not anything else as radius" do
+    expect{Circle.new('yummy pi')}.to raise_error
+  end 
+
+  #technically a circle of size 0 is a point which is a circle 
+  #but we will not allow it for s
+  it "takes only 1 non-negative argument as radius" do
+    expect{Circle.new(-1)}.to raise_error
+  end
+
+  it "can check the radius" do
+    expect(unit_circle.radius).to eq(1)
+  end
+
+
+  it "calculates the diameter" do
+    expect(unit_circle.diameter).to eq(2)
+  end
+
+
+  it "calculates the circumference" do
+    expect(unit_circle.circumference).to eq(6.28319)
+  end
+
+  it "calculates an area numerical response" do
+    expect(unit_circle.area).to eq(3.14159)
+  end
+
+  it "calculates an area word response" do
+    expect(unit_circle.area(true)).to eq("PI 1in^2" )
+  end
+
+  it "calculates an area word response" do
+    expect(Circle.new(5).area(true)).to eq("PI 5in^2" )
+  end
 
 
 end
